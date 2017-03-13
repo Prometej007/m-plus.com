38646d152b0c4d1cea31edbc2d51d45f68cae389
Nazar Danulyak <Nazar Danulyak>
2017-03-13 16:53:00 +0200

    add js css img

--- m-pluse/src/main/java/ua/m_pluse/controller/AdminController.java
+++ m-pluse/src/main/java/ua/m_pluse/controller/AdminController.java
@@ -1,8 +1,18 @@
 package ua.m_pluse.controller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AdminController {
 
+	@RequestMapping(value = { "/", "home" }, method = RequestMethod.GET)
+	public String home(Principal principal, Model model) {
+
+		return "";
+	}
 }
