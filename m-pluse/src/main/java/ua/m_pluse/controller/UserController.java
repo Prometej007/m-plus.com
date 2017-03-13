08ec249311097cf7ba9b6f5c3f34597ac082017b
Nazar Danulyak <Nazar Danulyak>
2017-03-13 20:36:19 +0200

    add gen img in gallery

--- m-pluse/src/main/java/ua/m_pluse/controller/UserController.java
+++ m-pluse/src/main/java/ua/m_pluse/controller/UserController.java
@@ -1,14 +1,52 @@
 package ua.m_pluse.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import ua.m_pluse.entity.Game;
+import ua.m_pluse.entity.Image;
+import ua.m_pluse.service.GameService;
+import ua.m_pluse.service.ImageService;
+import ua.m_pluse.service.UserService;
+
 @Controller
 public class UserController {
-	@RequestMapping(value = { "/", "home" }, method = RequestMethod.GET)
-	public String home() {
 
+	@Autowired
+	private UserService userService;
+
+	@Autowired
+	private ImageService imageService;
+
+	@Autowired
+	private GameService gameService;
+
+	@RequestMapping(value = { "/", "home" }, method = RequestMethod.GET)
+	public String home(Model model) {
+		List<Image> gallery = imageService.findAll();
+		List<Game> game = gameService.findAll();
+		String galleryString = "";
+		String gameString = "";
+		for (Image image : gallery) {
+
+			galleryString = "<a href='" + image.getPath() + "'> <img alt='" + image.getName() + "' src='"
+					+ image.getPath() + "'></a>";
+
+		}
+		// доробити------------------------------------------------------------------------------------------
+		for (Game gameI : game) {
+
+			gameString = "<a href='" + gameI.getPath() + "'> <img alt='" + gameI.getName() + "' src='"
+					+ gameI.getPath() + "'></a>";
+
+		}
+		//____________________________________________________________________________________________________
+		model.addAttribute(galleryString, "gallery");
 		return "home";
 	}
 
