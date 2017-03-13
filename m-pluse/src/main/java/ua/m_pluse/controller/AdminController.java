b192a0d8953fbdd27bd51e38f4c55c302848bb0d
Nazar Danulyak <Nazar Danulyak>
2017-03-13 18:01:30 +0200

    fix controller

--- m-pluse/src/main/java/ua/m_pluse/controller/AdminController.java
+++ m-pluse/src/main/java/ua/m_pluse/controller/AdminController.java
@@ -1,20 +1,13 @@
 package ua.m_pluse.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AdminController {
-
-	@RequestMapping(value = { "/", "home" }, method = RequestMethod.GET)
-	public String home() {
-
-		return "home";
-	}
-	@RequestMapping(value="loginpage",method=RequestMethod.GET)
-	public String loginpage(){
+	@RequestMapping(value = "loginpage", method = RequestMethod.GET)
+	public String loginpage() {
 		return "loginpage";
 	}
 }
