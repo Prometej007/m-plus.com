3b195b73aa191719987d23d17527f1a5c3163543
danul <danul@DESKTOP-0MKJDAG>
2017-03-16 11:56:44 +0200

    add

---
+++ m-pluse/src/main/webapp/js/LanguageButton.js
@@ -0,0 +1,9 @@
+function ButtonOff(){
+	document.getElementById('button-language-2').style.background="#929491";
+	document.getElementById('button-language-2').style.color="#595958";
+	
+}
+function ButtonOn(){
+	document.getElementById('button-language-2').style.background="#595958";
+	document.getElementById('button-language-2').style.color="#929491"
+}
