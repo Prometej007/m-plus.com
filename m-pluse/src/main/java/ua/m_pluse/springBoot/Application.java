9454ab55c9d9fe30da72d04c3baeb39d8b13b37b
alorind <cocomon@ukr.net>
2017-04-03 22:19:08 +0300

    asd

--- m-pluse/src/main/java/ua/m_pluse/springBoot/Application.java
+++ m-pluse/src/main/java/ua/m_pluse/springBoot/Application.java
@@ -1,24 +1,23 @@
 package ua.m_pluse.springBoot;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
-public class SpringBootWebApplication extends SpringBootServletInitializer {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringBootWebApplication.class);
-	}
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class Application{
+
 
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(SpringBootWebApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringApplicationBuilder.class);
-	}
+
 }
